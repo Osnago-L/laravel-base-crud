c44b73d970bc38a198ea33e6d1a12463fa92bcf4
Osnago-L <osna.brera@gmail.com>
2022-03-09 15:41:34 +0100

    end assignment

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,4 +1,11 @@
 @extends('template.template');
 @section('content')
+    <a href="{{route("comics.index")}}"><button>back to list</button></a>
     <h1>{{$comic->title}}</h1>
+    <p>{{$comic->description}}</p>
+    <img src="{{$comic->thumb}}" alt="">
+    <p>{{$comic->price}} $</p>
+    <p>{{$comic->series}}</p>
+    <p>{{$comic->sale_date}}</p>
+    <p>{{$comic->type}}</p>
 @endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+<h1>Navigation</h1>
+<ul>
+    <a href="{{route("comics.index")}}"><li>Comics List</li></a>
+</ul>
